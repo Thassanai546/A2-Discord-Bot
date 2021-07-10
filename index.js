ffc5bbf754286b719543d49782d0e22b504ba2a2
Thass <72495327+Thassanai546@users.noreply.github.com>
2021-07-10 19:23:29 +0000

    Made fucntion for selecting a random file.

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const fetch = require("node-fetch")
 const client = new Discord.Client()
 const db = new Database()
 
+getRandom = (directory) => { 
+  // Get a random file from a directory
+  let files = fs.readdirSync(directory)
+  let chosenFile = files[Math.floor(Math.random() * files.length)]
+  return {files: [directory + chosenFile]}
+}
+
 client.on("ready", () => {
   console.log(`Unit ${client.user.tag} online`)
   client.user.setActivity('the machine war', {type: 'COMPETING'})
@@ -19,7 +26,7 @@ client.on("message", msg => {
   }
 
   if (msg.content === "$about") {
-    msg.channel.send("```A2 was part of the experimental YoRHa squadron deployed on the Pearl Harbor Descent Mission during the 14th Machine War. A2 ended up being the last survivor of the YoRHa squadron after the mission, causing her to be declared a traitor and deserter by YoRHa Command due to the secrecy of the prototype YoRHa line. A2 then spends an unknown amount of time surviving alone, driven on by vengeance to defeat any machines and YoRHa units she comes upon on the way.```", {files: ['./assets/gifs/about/aboutA2.gif']})
+    msg.channel.send("```A2 was part of the experimental YoRHa squadron deployed on the Pearl Harbor Descent Mission during the 14th Machine War. A2 ended up being the last survivor of the YoRHa squadron after the mission, causing her to be declared a traitor and deserter by YoRHa Command due to the secrecy of the prototype YoRHa line. A2 then spends an unknown amount of time surviving alone, driven on by vengeance to defeat any machines and YoRHa units she comes upon on the way.```", getRandom('./assets/gifs/about/'))
   }
 
   if (msg.content === "hey a2") {
@@ -27,10 +34,7 @@ client.on("message", msg => {
   }
 
   if (msg.content === "$gif") {
-    var dir = './assets/gifs/random/'
-    let files = fs.readdirSync(dir)
-    let chosenFile = files[Math.floor(Math.random() * files.length)]
-    msg.channel.send({files: [dir + chosenFile]})
+    msg.channel.send(getRandom('./assets/gifs/random/'))
   }
 
   if (msg.content.startsWith("$avatar")) {

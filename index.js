4040e242786db9a811cb2262ac29d6dec79b5544
Thass <72495327+Thassanai546@users.noreply.github.com>
2021-07-10 23:52:14 +0000

    Improved "about" command

--- index.js
+++ index.js
@@ -48,7 +48,21 @@ client.on("message", msg => {
   }
 
   if (msg.content === "$about") {
-    msg.channel.send("```A2 was part of the experimental YoRHa squadron deployed on the Pearl Harbor Descent Mission during the 14th Machine War. A2 ended up being the last survivor of the YoRHa squadron after the mission, causing her to be declared a traitor and deserter by YoRHa Command due to the secrecy of the prototype YoRHa line. A2 then spends an unknown amount of time surviving alone, driven on by vengeance to defeat any machines and YoRHa units she comes upon on the way.```", getRandom('./assets/gifs/about/'))
+    let about_embed = new Discord.MessageEmbed()
+    .setTitle("YoRHa Type A No.2")
+    .setDescription("```A2 was part of the experimental YoRHa squadron deployed on the Pearl Harbor Descent Mission during the 14th Machine War. A2 ended up being the last survivor of the YoRHa squadron after the mission, causing her to be declared a traitor and deserter by YoRHa Command due to the secrecy of the prototype YoRHa line. A2 then spends an unknown amount of time surviving alone, driven on by vengeance to defeat any machines and YoRHa units she comes upon on the way.```")
+    .setImage("https://github.com/Thassanai546/A2-Discord-Bot/blob/master/assets/gifs/about/53-corruption_(57).jpg?raw=true")
+    .setURL("https://nier.fandom.com/wiki/YoRHa_Type_A_No.2")
+    .addFields(
+		  { name:'Occupation', value: 'YoRHa (Formerly)', inline: true },
+      { name: 'Height', value: '168cm (including heels)', inline: true},
+      { name: 'Weight', value: '139.2kg', inline: true },
+    )
+    msg.channel.send(about_embed)
+  }
+
+  if (msg.content.startsWith("$personality")) {
+    msg.channel.send("A2 is initially depicted as mysterious and taciturn. However, it is eventually shown through her interactions with Pod 042 that she possesses an aggressive and fiery personality. She has a propensity towards reckless behavior as she will dive head-long into dangerous situations without consideration for strategic evaluations or her own personal safety. When speaking to others, she tends to be blunt, impatient and foul-mouthed.")
   }
 
   if (msg.content === "$gif") {
@@ -58,10 +72,10 @@ client.on("message", msg => {
   if (msg.content.startsWith("$avatar")) {
     try {
       let member = msg.mentions.users.first();
-      let embed = new Discord.MessageEmbed()
+      let avatar_embed = new Discord.MessageEmbed()
       .setAuthor(member.username)
       .setImage(member.avatarURL({ format: 'jpg', size: 512}));
-      msg.channel.send(embed)
+      msg.channel.send(avatar_embed)
     } catch(err) {
       msg.channel.send("Sorry, I could not find that user.")
     }
